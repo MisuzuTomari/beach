46589a7663b97ccd79fa327eb583e503bc6b2f19
Misuzu Tomari <mee.15.751@gmail.com>
2021-05-21 13:07:21 +0900

    編集中

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,21 +29,20 @@ const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'vertical',
     loop: true,
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination-beach',
+    },
   
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next-beach',
+      prevEl: '.swiper-button-prev-beach',
+    },
   
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-//ここまでスワイパー
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar-beach',
+    },
+  });
